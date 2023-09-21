1286199f6320ebd390dba01c11c06dbad5ea13f7
Molham Al khodari <alkhodari@drherwig.de>
2023-09-21 09:01:57 +0200

    Configuring SQL Server

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// DbContext configuration
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+
 
 var app = builder.Build();
 
